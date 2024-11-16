4a9c276ec57854b2745b0e720fa963bc097fb5f3
Justin Pyron <justinpyron@gmail.com>
2024-11-16 08:56:44 -0600

    Create main backend for the RAG system

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
+
 import jinja2
+from openai import OpenAI
 
 from knowledge_base import KnowledgeBase
 
-prompt_skeleton = """
+API_KEY = os.environ["OPENAI_API_KEY__PRESIDENTS_RAG"]
+OPENAI_MODEL = "gpt-4o-mini"
+PROMPT_SKELETON = """
 QUESTION:
 {{ query }}
 
@@ -20,16 +25,31 @@ Answer the QUESTION above using the list of documents in DOCUMENTS. Your answer 
 class PresidentsRAG:
     def __init__(self) -> None:
         self.knowledge_base = KnowledgeBase()
-        self.prompt_template = jinja2.Template(prompt_skeleton)
+        self.prompt_template = jinja2.Template(PROMPT_SKELETON)
+        self.openai_client = OpenAI(api_key=API_KEY)
 
     def ping_openai(self, prompt: str) -> str:
-        pass
+        completion = self.openai_client.chat.completions.create(
+            model=OPENAI_MODEL,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant with close attention to detail.",
+                },
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+        )
+        return completion.choices[0].message.content
 
     def ask(
         self,
         query: str,
         top_k: int = 5,
     ):
-        response = self.knowledge_base.fetch_similar_documents(query, top_k)
-        prompt = self.prompt_template.render(query=query, documents=response["text"])
-        return prompt
+        knowledge = self.knowledge_base.fetch_similar_documents(query, top_k)
+        prompt = self.prompt_template.render(query=query, documents=knowledge["text"])
+        answer = self.ping_openai(prompt)
+        return answer
