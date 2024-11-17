e7d04fcd5408fbf7cf2cecf3ac9c91b5d94aaa5b
Justin Pyron <justinpyron@gmail.com>
2024-11-17 09:40:00 -0600

    Finish the app

--- app.py
+++ app.py
@@ -2,6 +2,37 @@ import streamlit as st
 
 from main import PresidentsRAG
 
+how_it_works = """
+## 💫 Ask a question about a President 💫
+
+An answer based on a knowledge base of documents is provided using Retrieval Augmented Generation (RAG).
+
+## Knowledge Base
+Answers are based on the Wikipedia articles of all US Presidents and Secretaries of State.
+These articles were scraped using [Wikipedia's OpenSearch API](https://www.mediawiki.org/wiki/API:Opensearch).
+The `text/` and `chunks/` directories in the GitHub contain the raw text files.
+
+## Models
+1. A model to create vector embeddings of documents and questions. I use a [Sentence Transformer](https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2).
+2. A generative chatbot to answer context-enriched queries. I use [OpenAI's gpt-4o-mini](https://platform.openai.com/docs/models#gpt-4o-mini).
+
+## Workflow
+In simple terms, the RAG system transforms a user's query into a prompt enriched with context from the knowledge base.
+
+##### Offline
+1. Create a knowledge base of documents.
+2. Create a vector embedding of each document in the knowledge base.
+
+##### At inference time
+1. Create a vector embedding of the question you want to ask.
+2. Apply cosine similarity to the embeddings to find the documents most similar to the question.
+3. Create a prompt that supplements the question with the most similar documents.
+4. Submit the prompt to the generative chatbot to generate an answer.
+
+## Full Details
+See 👉 [GitHub](https://github.com/justinpyron/presidents-rag).
+"""
+
 
 @st.cache_resource
 def load_rag() -> PresidentsRAG:
@@ -18,8 +49,8 @@ def build_sources_string(
         zip(filenames, documents, similarities)
     ):
         sources += (
-            f"## Document {i + 1}\n"
-            f"#### `{filename}` (cosine similarity = {similarity:.3f})\n"
+            f"### Document {i + 1}\n"
+            f"##### `{filename}` (cosine similarity = {similarity:.3f})\n"
             f"{text}\n\n"
         )
     return sources
@@ -29,15 +60,15 @@ st.set_page_config(page_title="Presidents RAG", layout="centered", page_icon="�
 rag = load_rag()
 st.title("Presidents RAG")
 with st.expander("How it works"):
-    st.markdown("TODO")
+    st.markdown(how_it_works)
 query = st.text_area("Ask a question", "")
-# TODO: slider for minimum cosine similarity, in [0,1]
-# TODO: slider for number of documents to consider
 if st.button("Submit", type="primary", use_container_width=True):
     knowledge, answer = rag.ask(query)
     st.write(answer)
     with st.expander("Sources"):
         sources = build_sources_string(
-            knowledge["filename"], knowledge["text"], knowledge["cosine_similarity"]
+            knowledge["filename"],
+            knowledge["text"],
+            knowledge["cosine_similarity"],
         )
         st.markdown(sources)
