914f05f7be681159fade02afda180a33cdfe1729
Justin Pyron <justinpyron@gmail.com>
2024-12-19 21:19:26 -0600

    Use vector store

--- app.py
+++ app.py
@@ -39,23 +39,6 @@ def load_rag() -> PresidentsRAG:
     return PresidentsRAG()
 
 
-def build_sources_string(
-    filenames: list[str],
-    documents: list[str],
-    similarities: list[float],
-) -> str:
-    sources = ""
-    for i, (filename, text, similarity) in enumerate(
-        zip(filenames, documents, similarities)
-    ):
-        sources += (
-            f"### Document {i + 1}\n"
-            f"##### `{filename}` (cosine similarity = {similarity:.3f})\n"
-            f"{text}\n\n"
-        )
-    return sources
-
-
 st.set_page_config(page_title="Presidents RAG", layout="centered", page_icon="🇺🇸")
 rag = load_rag()
 st.title("US Presidents RAG 🇺🇸")
@@ -63,12 +46,8 @@ with st.expander("How it works"):
     st.markdown(how_it_works)
 query = st.text_area("Ask a question", "")
 if st.button("Submit", type="primary", use_container_width=True):
-    knowledge, answer = rag.ask(query, top_k=10)
+    answer, documents = rag.ask(query, top_k=10)
     st.write(answer)
     with st.expander("Sources"):
-        sources = build_sources_string(
-            knowledge["filename"],
-            knowledge["text"],
-            knowledge["cosine_similarity"],
-        )
-        st.markdown(sources)
+        pretty_docs = [f"### Document {i+1}\n{doc}" for i, doc in enumerate(documents)]
+        st.markdown("\n\n".join(pretty_docs))
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 
+import chromadb
 import jinja2
 from openai import OpenAI
 
+from create_vector_store import VECTOR_STORE_NAME, VECTOR_STORE_PATH
 from knowledge_base import KnowledgeBase
 
 API_KEY = os.environ["OPENAI_API_KEY__PRESIDENTS_RAG"]
@@ -24,7 +26,8 @@ Answer the QUESTION above using the list of documents in DOCUMENTS. Your answer 
 
 class PresidentsRAG:
     def __init__(self) -> None:
-        self.knowledge_base = KnowledgeBase()
+        client = chromadb.PersistentClient(path=VECTOR_STORE_PATH)
+        self.vector_store = client.get_collection(name=VECTOR_STORE_NAME)
         self.prompt_template = jinja2.Template(PROMPT_SKELETON)
         self.openai_client = OpenAI(api_key=API_KEY)
 
@@ -49,7 +52,8 @@ class PresidentsRAG:
         query: str,
         top_k: int = 5,
     ) -> tuple[dict, str]:
-        knowledge = self.knowledge_base.fetch_similar_documents(query, top_k)
-        prompt = self.prompt_template.render(query=query, documents=knowledge["text"])
+        result = self.vector_store.query(query_texts=query, n_results=top_k)
+        documents = result["documents"][0]
+        prompt = self.prompt_template.render(query=query, documents=documents)
         answer = self.ping_openai(prompt)
-        return knowledge, answer
+        return answer, documents
