49ed11a105d39fc79c62fcd90ccd03ca68d950d1
Justin Pyron <justinpyron@gmail.com>
2024-12-21 15:50:31 -0600

    Function to create vector store pickle

--- vector_store.py
+++ vector_store.py
@@ -1,8 +1,73 @@
+import os
+import pickle
+
 import numpy as np
 import torch
-from create_artifacts import MODEL_NAME, read_pickle
+from langchain_community.document_loaders import TextLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from scipy.spatial.distance import cdist
 from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
+
+MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+DOCUMENTS_ROOT = "text"
+
+
+def write_pickle(
+    obj: any,
+    filename: str,
+) -> None:
+    with open(filename, "wb") as handle:
+        pickle.dump(obj, handle)
+
+
+def read_pickle(filename: str) -> any:
+    with open(filename, "rb") as handle:
+        return pickle.load(handle)
+
+
+def chunk_documents(documents_folder: str) -> list[Document]:
+    filenames = sorted(
+        [os.path.join(documents_folder, file) for file in os.listdir(documents_folder)]
+    )
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=900,
+        chunk_overlap=300,
+        add_start_index=True,
+    )  # With chunk_size=900, 99%+ of chunks fit inside all-MiniLM-L6-v2 context window
+    chunks = list()
+    for file in filenames:
+        doc = TextLoader(file).load()
+        chunks += text_splitter.split_documents(doc)
+    return chunks
+
+
+def create_vector_store(
+    documents_folder: str,
+    filename: str,
+) -> None:
+    sentence_transformer = SentenceTransformer(MODEL_NAME)
+    chunks = chunk_documents(documents_folder)
+    ids = list()
+    texts = list()
+    embeddings = list()
+    for chunk in tqdm(chunks[:50]):
+        id = (
+            f'{chunk.metadata["source"].split("/")[-1]}:{chunk.metadata["start_index"]}'
+        )
+        text = chunk.page_content
+        with torch.no_grad():
+            embedding = sentence_transformer.encode(text)
+        ids.append(id)
+        texts.append(text)
+        embeddings.append(embedding)
+    vector_store = {
+        "ids": ids,
+        "texts": texts,
+        "embeddings": np.vstack(embeddings),
+    }
+    write_pickle(vector_store, filename)
 
 
 class KnowledgeBase:
